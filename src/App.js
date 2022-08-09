f86a49f6305dd358cae691cdffe9bb59017c4946
Sam Kobe (Web Ok) <samuelkobe@gmail.com>
2022-08-09 11:37:33 -0700

    added assets, changed index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDr9KdV5LuPYVB4JU0lFo6Ykp0kz4s1YVE",
+  authDomain: "sun-west-helicopters.firebaseapp.com",
+  projectId: "sun-west-helicopters",
+  storageBucket: "sun-west-helicopters.appspot.com",
+  messagingSenderId: "179679203541",
+  appId: "1:179679203541:web:4fba1c9044af6f3df9125c",
+  measurementId: "G-Q8CF59G5T1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
